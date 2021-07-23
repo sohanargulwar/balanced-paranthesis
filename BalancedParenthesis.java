c8e2166afb0cddee2b563303eaccad78b7d89c20
sohanargulwar <52895666+sohanargulwar@users.noreply.github.com>
2021-07-23 13:02:37 -0400

    BalancedParenthesis

---
+++ BalancedParenthesis.java
@@ -0,0 +1,31 @@
+import java.util.Stack;
+
+public class BalancedParenthesis {
+
+    private static Stack s = new Stack();
+    public static void main(String[] args) {
+
+        System.out.println(isBalanced("()"));
+        System.out.println(isBalanced("()()()(())"));
+        System.out.println(isBalanced("()()())"));
+        System.out.println(isBalanced("(()(()()()"));
+        System.out.println(isBalanced("(()(()())"));
+    }
+
+    public static boolean isBalanced(final String stringToCheck) {
+
+        for(int i=0;i<stringToCheck.length(); i++) {
+
+            if (stringToCheck.charAt(i) == '(') {
+                s.push(stringToCheck.charAt(i));
+            } else if (stringToCheck.charAt(i) == ')'){
+                if (s.isEmpty()) {
+                    return false;
+                } else{
+                    s.pop();
+                }
+            }
+        }
+        return s.empty();
+    }
+}
